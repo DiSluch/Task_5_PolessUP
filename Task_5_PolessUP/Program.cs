73c355852d7e2778c5019cba15b5ba436215163a
Дима Случак <pripru916@gmail.com>
2023-04-18 22:24:56 +0300

    Добавьте файлы проекта.

---
+++ Task_5_PolessUP/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_5_PolessUP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите размер массива:");
+            int n = int.Parse(Console.ReadLine());
+
+            int[] nums = new int[n];
+
+            Console.WriteLine("Введите элементы массива:");
+
+            for (int i = 0; i < n; i++)
+            {
+                nums[i] = int.Parse(Console.ReadLine());
+            }
+
+            int max = nums[0];
+
+            int j = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                if (nums[i] > max)
+                {
+                    max = nums[i];
+                    j = i;
+                }
+            }
+
+            Console.WriteLine($"Максимальный индекс числа: {j}");
+
+            Console.ReadKey();
+        }
+    }
+}
